4eed1636acde5b0379e126b8035590b722019ee8
Ville Koskela <vkoskela@gmail.com>
2021-09-14 21:03:05 +0300

    working with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/Person')
 const app = express()
 
 morgan.token('postBody', (req) => JSON.stringify(req.body))
@@ -10,7 +12,7 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
 app.use(express.static('build'))
 
-let persons = [
+let persons2 = [
     {
         id: 1,
         name: "Maukka Perusjätkä",
@@ -33,44 +35,74 @@ let persons = [
     }
 ]
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has ${persons.length} people in it</p><p>${new Date()}</p>`)
+const getAll = async () => {
+    try {
+        const response = await Person.find({})
+        return response
+    } catch (error) {
+        console.log(error)
+        return []
+    }
+}
+
+app.get('/info', async (req, res) => {
+    try {
+        const persons = await getAll()
+        res.send(`<p>Phonebook has ${persons.length} people in it</p><p>${new Date()}</p>`)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', async (req, res) => {
+    try {
+        const persons = await getAll()
+        res.json(persons)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', async (req, res) => {
     if (!req.body.name || !req.body.number) {
         // either name or number (or both) are missing
         res.status(400).json({ error: 'required information missing' })
         return
     }
+    const persons = await getAll()
     if (persons.some(p => p.name === req.body.name)) {
         // name already exists
         res.status(400).json({ error: 'name already exists in the phonebook' })
     }
     const name = req.body.name
     const number = req.body.number
-    const id = Math.floor(Math.random() * 1000000)
-    const person = { id: id, name: name, number: number }
-    persons = persons.concat(person)
-
-    res.json(person)
+    const person = new Person({ name: name, number: number })
+    try {
+        const savedPerson = await person.save()
+        res.json(savedPerson)
+    } catch (error) {
+        // most likely the name already exists in the database
+        console.log(error)
+        res.status(400).json({ error: 'unknown error' })
+    }
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(p => p.id === Number(req.params.id))
-    if (person)
+app.get('/api/persons/:id', async (req, res) => {
+    try {
+        const person = await Person.findById(req.params.id)
         res.json(person)
-    else
-        res.status(404).end()
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const newPersons = persons.filter(p => p.id !== Number(req.params.id))
-    persons = newPersons
+app.delete('/api/persons/:id', async (req, res) => {
+    try {
+        const response = await Person.deleteOne({ _id: req.params.id })
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
     res.status(204).end()
 })
 
@@ -81,12 +113,12 @@ app.put('/api/persons/:id', (req, res) => {
         res.status(400).json({ error: 'required information missing' })
         return
     }
-    if (persons.find(p => p.id === Number(req.params.id))) {
+    if (persons2.find(p => p.id === Number(req.params.id))) {
         const name = req.body.name
         const number = req.body.number
         const id = Number(req.params.id)
         const person = { id: id, name: name, number: number }
-        persons = persons.map(p => p.id !== id ? p : person)
+        persons2 = persons2.map(p => p.id !== id ? p : person)
 
         res.json(person)
     }
@@ -94,7 +126,7 @@ app.put('/api/persons/:id', (req, res) => {
     res.status(404).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     // what ever we wanna do here
     console.log(`server started on port ${PORT}`)
