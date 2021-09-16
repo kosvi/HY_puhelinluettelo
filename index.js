d69725953f64bada50389d97f06855c1843cdd6d
Ville Koskela <vkoskela@gmail.com>
2021-09-16 20:56:07 +0300

    competed 3.18

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app = express()
 morgan.token('postBody', (req) => JSON.stringify(req.body))
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
-app.use(express.static('build'))
+
+
 
 const getAll = async () => {
     try {
@@ -22,53 +24,47 @@ const getAll = async () => {
     }
 }
 
-app.get('/info', async (req, res) => {
+const checkBody = (req, res) => {
+    if (!req.body.name || !req.body.number) {
+        res.status(400).json({ error: 'required information missing' })
+        return -1
+    }
+    return 0
+}
+
+app.get('/info', async (req, res, next) => {
     try {
         const persons = await getAll()
         res.send(`<p>Phonebook has ${persons.length} people in it</p><p>${new Date()}</p>`)
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 })
 
-app.get('/api/persons', async (req, res) => {
+app.get('/api/persons', async (req, res, next) => {
     try {
         const persons = await getAll()
         res.json(persons)
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 })
 
-app.post('/api/persons', async (req, res) => {
-    if (!req.body.name || !req.body.number) {
-        // either name or number (or both) are missing
-        res.status(400).json({ error: 'required information missing' })
+app.post('/api/persons', async (req, res, next) => {
+    if (checkBody(req, res)) {
         return
     }
-    // probably not needed -> frontend already handles this
-    // and in case user manages to send existing name, it will fail later
-    // in fact, this isn't even a good way to check this as name could be added/removed
-    // between this check and save()
-    const persons = await getAll()
-    if (persons.some(p => p.name === req.body.name)) {
-        // name already exists
-        res.status(400).json({ error: 'name already exists in the phonebook' })
-    }
-    const name = req.body.name
-    const number = req.body.number
-    const person = new Person({ name: name, number: number })
+    const person = new Person({ name: req.body.name, number: req.body.number })
     try {
         const savedPerson = await person.save()
         res.json(savedPerson)
     } catch (error) {
         // most likely the name already exists in the database
-        console.log(error)
-        res.status(400).json({ error: 'unknown error' })
+        next(error)
     }
 })
 
-app.get('/api/persons/:id', async (req, res) => {
+app.get('/api/persons/:id', async (req, res, next) => {
     try {
         const person = await Person.findById(req.params.id)
         if (person)
@@ -77,35 +73,33 @@ app.get('/api/persons/:id', async (req, res) => {
             res.status(404).end()
         }
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 })
 
-app.delete('/api/persons/:id', async (req, res) => {
+app.delete('/api/persons/:id', async (req, res, next) => {
     try {
-        const response = await Person.deleteOne({ _id: req.params.id })
+        const response = await Person.findByIdAndDelete(req.params.id)
         console.log(response)
+        res.status(204).end()
     } catch (error) {
-        console.log(error)
+        next(error)
     }
-    res.status(204).end()
 })
 
-app.put('/api/persons/:id', async (req, res) => {
-    // little bit copy & paste here, I will refactor later
-    if (!req.body.name || !req.body.number) {
-        // either name or number (or both) are missing
-        res.status(400).json({ error: 'required information missing' })
+app.put('/api/persons/:id', async (req, res, next) => {
+    if (checkBody(req, res)) {
         return
     }
     const person = { _id: req.params.id, name: req.body.name, number: req.body.number }
     try {
         const response = await Person.findByIdAndUpdate(req.params.id, person)
-        console.log(response)
-        res.json(await Person.findById(req.params.id))
+        if (response)
+            res.json(response)
+        else
+            res.status(404).end()
     } catch (error) {
-        console.log(error)
-        res.status(404).end()
+        next(error)
     }
 })
 
@@ -114,3 +108,20 @@ app.listen(PORT, () => {
     // what ever we wanna do here
     console.log(`server started on port ${PORT}`)
 })
+
+const errorHandler = (err, req, res, next) => {
+    console.log('Error name: ', err.name)
+    console.log(err.message)
+    if (err.name === 'CastError')
+        return res.status(400).send({ error: 'malformed id' })
+    if (err.code === 11000)
+        return res.status(400).send({ error: 'name already exists in the phonebook' })
+    next(err)
+}
+app.use((err, req, res, next) => {
+    // log all errors
+    console.log('Server experienced an error')
+    console.log(err)
+    next(err)
+})
+app.use(errorHandler)
